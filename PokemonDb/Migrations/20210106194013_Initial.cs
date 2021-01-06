a2f6b1c0fe1a72a0a37060fc950fa53fc6dd192f
agatakolohe <agatakolohe@gmail.com>
2021-01-06 11:44:42 -0800

    create db, initial migration, db update
    Co-authored-by: Garrett Dean Brown <gman9mm@live.com
    Co-authored-by: Jeff Chiu <jeffchiudev@gmail.com>

---
+++ PokemonDb/Migrations/20210106194013_Initial.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PokemonDb.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pokemons",
+                columns: table => new
+                {
+                    PokemonId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    PokemonDescription = table.Column<string>(nullable: true),
+                    PokemonName = table.Column<string>(nullable: true),
+                    PokemonGender = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pokemons", x => x.PokemonId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PokeTypes",
+                columns: table => new
+                {
+                    PokeTypeId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    PokeTypeName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PokeTypes", x => x.PokeTypeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pokedex",
+                columns: table => new
+                {
+                    PokedexId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    PokemonId = table.Column<int>(nullable: false),
+                    PokeTypeId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pokedex", x => x.PokedexId);
+                    table.ForeignKey(
+                        name: "FK_Pokedex_PokeTypes_PokeTypeId",
+                        column: x => x.PokeTypeId,
+                        principalTable: "PokeTypes",
+                        principalColumn: "PokeTypeId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Pokedex_Pokemons_PokemonId",
+                        column: x => x.PokemonId,
+                        principalTable: "Pokemons",
+                        principalColumn: "PokemonId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pokedex_PokeTypeId",
+                table: "Pokedex",
+                column: "PokeTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pokedex_PokemonId",
+                table: "Pokedex",
+                column: "PokemonId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pokedex");
+
+            migrationBuilder.DropTable(
+                name: "PokeTypes");
+
+            migrationBuilder.DropTable(
+                name: "Pokemons");
+        }
+    }
+}
